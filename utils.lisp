1af272ed077795c5f692f0e77681bddbf103cc35
jgart <jgart@dismail.de>
2024-06-16 01:58:44 -0500

    Add shuffle function

--- utils.lisp
+++ utils.lisp
@@ -2,3 +2,14 @@
 
 (defun concat (&rest strings)
   (apply #'concatenate 'string strings))
+
+(defun shuffle (list)
+  (let* ((random-index (random (1- (length list))))
+	 (random-value (nth random-index list))
+	 (first-item-in-list (first list)))
+    (setf (first list) random-value
+	  (nth random-index list) first-item-in-list))
+  list)
+
+
+

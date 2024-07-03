23677ce8b391ca1fb5368c176720f1eae5e0ae36
jgart <jgart@dismail.de>
2024-07-03 17:04:43 -0500

    Add help flag

--- src/main.lisp
+++ src/main.lisp
@@ -3,6 +3,9 @@
 (defun main (&optional
                (input-filepath (first (uiop:command-line-arguments)))
                (output-filepath "output.ly"))
+  (when (member "--help" (uiop:command-line-arguments) :test #'string=)
+    (format t "Usage: compose [FILE]...~%")
+    (uiop:quit))
   (setf *random-state* (make-random-state t))
   (write-music input-filepath output-filepath)
   (lilypond output-filepath))

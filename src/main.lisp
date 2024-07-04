a23c5942509aaedb56828e90117babea4a988e38
jgart <jgart@dismail.de>
2024-07-03 22:10:37 -0500

    Add command line checks

--- src/main.lisp
+++ src/main.lisp
@@ -3,12 +3,30 @@
 (defun check-arg (arg)
   (member arg (uiop:command-line-arguments) :test #'string=))
 
+(defun print-help ()
+  (format t "Usage: compose [FILE]...~%"))
+
+(defun check-help-flag ()
+  (when (check-arg "--help")
+    (print-help)
+    (uiop:quit)))
+
+(defun check-too-many-args ()
+  (when (> (length (uiop:command-line-arguments)) 1)
+    (print-help)
+    (uiop:quit)))
+
+(defun check-missing-file-arg (file)
+  (when (and file
+             (not (file-exists-p file)))
+    (format *error-output* "`~A' does not exist.~%" file)
+    (uiop:quit)))
+
 (defun main (&optional
                (input-filepath (first (uiop:command-line-arguments)))
                (output-filepath "output.ly"))
-  (when (or (check-arg "--help")
-            (null input-filepath))
-    (format t "Usage: compose [FILE]...~%")
-    (uiop:quit))
+  (check-help-flag)
+  (check-missing-file-arg input-filepath)
+  (check-too-many-args)
   (write-music input-filepath output-filepath)
   (lilypond output-filepath))
--- src/package.lisp
+++ src/package.lisp
@@ -1,4 +1,4 @@
 (defpackage :jgart.compose
   (:use cl)
-  (:import-from #:uiop #:split-string)
+  (:import-from #:uiop #:split-string #:file-exists-p)
   (:export #:main))
